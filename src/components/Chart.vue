3b6f0cc22d9a5eb9c02b56c1474a5b6ae018e60f
Ivan Castro <ivan20cast@gmail.com>
2022-01-25 17:55:30 -0500

    graficos terminados

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,99 +1,96 @@
 <template>
   <div>
     <div>
-      <select name="select" v-model="seleccionado">
+      <select
+        name="select"
+        v-model="anioSeleccionado"
+        @change="cambiarAnio($event)"
+        class="form-select form-select-lg mb-3"
+        aria-label=".form-select-lg example"
+      >
         <option value="2017">2017</option>
         <option value="2018">2018</option>
         <option value="2019">2019</option>
         <option value="2020">2020</option>
         <option value="2021">2021</option>
       </select>
-      <div>
-        {{ seleccionado }}
+      <div
+        class="btn-group"
+        role="group"
+        aria-label="Basic mixed styles example"
+      >
+        <button
+          type="button"
+          class="btn btn-danger"
+          v-on:click="cambiarGrafico('ColumnChart')"
+        >
+          Gráfico de Barras
+        </button>
+        <button
+          :type="tipoGrafico"
+          class="btn btn-warning"
+          v-on:click="cambiarGrafico('PieChart')"
+        >
+          Gráfico de Pie
+        </button>
+        <button
+          type="button"
+          class="btn btn-success"
+          v-on:click="cambiarGrafico('LineChart')"
+        >
+          Gráfico de Línea
+        </button>
       </div>
-    </div>
-    <canvas id="grafico"></canvas>
-    <div>Lista de exportaciones</div>
-    <div v-for="(expor, index) in exportaciones" v-bind:key="index">
-      {{ expor }}
+    
+      <GChart
+        :type="tipoGrafico"
+        :data="chartData"
+        :options="chartOptions"
+        style="width: 900px; height: 500px"
+      />
+      
     </div>
   </div>
 </template>
 
 <script>
-import Chart from "chart.js";
 import axios from "axios";
+import { GChart } from "vue-google-charts";
 export default {
   name: "Chart",
+  components: {
+    GChart,
+  },
   data() {
     return {
       exportaciones: null,
-      seleccionado: 2017,
-      dataChart: {
-        type: "bar",
-        data: {
-          labels: [],
-          datasets: [
-            {
-              label: "# of Votes",
-              data: [],
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
+      anioSeleccionado: 2017,
+      tipoGrafico: "ColumnChart",
+      chartData: [],
+      chartOptions: {
+        title: "Exportaciones",
+        legend: "left",
       },
     };
   },
   mounted() {
     this.traerDatos();
-    this.mostrarGrafico();
   },
   methods: {
-    async traerDatos() {
-      let listaPaises = [];
-      let listaExport = [];
-      await axios.get("http://localhost:3000/api/cacao").then((response) => {
-        this.exportaciones = response.data.exportaciones;
-        // for (let exportac of this.exportaciones) {
-        //   this.dataChart.data.labels.push(exportac["pais"]);
-        //   this.dataChart.data.datasets.data.push(exportac["num_export"]);
-        // }
-
-        this.exportaciones.forEach((exportacion) => {
-          //console.log(exportacion["pais"], exportacion["num_export"]);
-          listaPaises.push(exportacion["pais"]);
-          listaExport.push(exportacion["num_export"]);
+    traerDatos(anio = 2017) {
+      axios
+        .get(`http://localhost:3000/api/cacao?anio=${anio}`)
+        .then((response) => {
+          this.exportaciones = response.data.exportaciones;
+          this.chartData = this.exportaciones;
         });
-      });
-      this.dataChart.data.labels = listaPaises;
-      this.dataChart.data.datasets[0].data = listaExport;
     },
-    mostrarGrafico() {
-      const chart = document.getElementById("grafico");
-      new Chart(chart, this.dataChart);
+    cambiarAnio(evento) {
+      this.anioSeleccionado = evento.target.value;
+      this.traerDatos(this.anioSeleccionado);
+    },
+    cambiarGrafico(tipo) {
+      this.tipoGrafico = tipo;
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import VueGoogleCharts from 'vue-google-charts';
+
+Vue.use(VueGoogleCharts)
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
