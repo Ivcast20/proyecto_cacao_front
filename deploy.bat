0703c663eb031329a1f4a365a724a08900f96571
Ivan Castro <ivan20cast@gmail.com>
2022-02-16 14:35:18 -0500

    New Deployment

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,9 @@
 set -e 
 npm run build
-cd dist
+cd .\dist\
 git init
 git add -A 
 git comit -m 'New Deployment'
-git push -f git@github.com:Ivcast20/proyecto_cacao_front.git master:gh-pages
+git push -f git@github.com:Ivcast20/proyecto_cacao_front.git main:gh-pages
 
 cd -
